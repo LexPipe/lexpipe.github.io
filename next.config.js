0d751c6af3ffc96fd6af34ae29c50b5719e83293
speedplane <michael.sander@lexpipe.com>
2024-03-09 15:14:17 -0500

    Add two build parameters and see if it makes a difference.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     output: "export",
     // Keep compatibility with static sites.
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    // Use the name of your repository as the basePath and assetPrefix
+    basePath: '/lexpipe.github.io',
+    assetPrefix: '/lexpipe.github.io/',
 }
 
 module.exports = nextConfig
