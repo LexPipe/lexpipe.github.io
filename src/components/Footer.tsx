6d961e6347dada2decbffecf29acb9c60c79e6fa
speedplane <michael.sander@lexpipe.com>
2024-03-11 14:42:07 -0400

    Add a simple Trustshare link into the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,18 +10,19 @@ export function Footer() {
       <Container>
         <div className="py-4">
           <Logo className="h-10 w-auto"/>
-          <p className="mt-6 text-sm text-slate-500 sm:mt-0">
+          <p className="mt-6 text-sm text-slate-500 sm:mt-0 inline-block">
             &copy; {new Date().getFullYear()} LexPipe, Inc.
           </p>
 
-          {/*}
-          <nav className="mt-10 text-sm" aria-label="quick links">
-            <div className="-my-1 flex justify-center gap-x-6">
-              <NavLink href="#features">Features</NavLink>
+
+          <nav className="inline-block ml-10 mt-10 text-sm" aria-label="quick links">
+            <div className="my-1 flex justify-center gap-x-6">
+            <NavLink href="https://lexpipe.trustshare.com/home">TrustShare</NavLink>
+              {/*}    <NavLink href="#features">Features</NavLink>
               <NavLink href="#testimonials">Testimonials</NavLink>
-              <NavLink href="#pricing">Pricing</NavLink>
+              <NavLink href="#pricing">Pricing</NavLink>*/}
             </div>
-          </nav>*/}
+          </nav>
         </div>
         {/*
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
