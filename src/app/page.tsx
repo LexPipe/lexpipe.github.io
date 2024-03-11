2907398628493813aeecbd606f8bc3531e34825a
speedplane <michael.sander@lexpipe.com>
2024-03-11 15:20:14 -0400

    Add a better popup image for LexPipe.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,25 @@ import { Header } from '@/components/Header'
 import { HeroAngled } from "@/components/HeroAngled";
 import {FeatureImageManyToMany, FeatureImageHubSpoke} from "@/components/FeatureImageLeft";
 import ValueProposition from "@/components/ValueProposition";
+import Logo1000 from "@/images/LexPipe-White-Sq-1000-Pipes.png";
+import type {Metadata} from "next";
 
+export const metadata: Metadata = {
+    title: {
+        template: 'LexPipe',
+        default: 'LexPipe - Legal workflow automation for law firms',
+    },
+    // Add the logo to the openGraph
+    openGraph : {
+        images: [{
+            url : Logo1000.src,
+            width : Logo1000.width,
+            height : Logo1000.height,
+        }],
+    },
+    description:
+        'LexPipe transforms the way law firms and corporates use legal information to develop new business and manage matters.',
+}
 
 
 export default function Home() {
