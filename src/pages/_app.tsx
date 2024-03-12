9d48d3c8dba97c017720ba61eae2f4c90ce5fc52
speedplane <michael.sander@lexpipe.com>
2024-03-12 15:20:47 -0400

    Remove reliance on RootLayout.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/tailwind.css';
 import { AppProps } from 'next/app';
-import {RootLayout} from '@/components/RootLayout';
 import { inter, lexend } from '@/fonts';
 
 export default function LexPipeSite({ Component, pageProps }: AppProps) {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,26 @@
-import {RootLayout, RootDocumentLayout} from '@/components/RootLayout'
 import Document, {Head, Html, Main, NextScript} from 'next/document'
+import {GoogleTagManager} from "@next/third-parties/google";
+import clsx from "clsx";
+import {inter, lexend} from "@/fonts";
 
+export function RootDocumentLayout() {
+    return (
+        <Html lang="en"
+              className={clsx('h-full scroll-smooth bg-white antialiased',
+                  inter.variable,
+                  lexend.variable,
+              )}>
+            <Head>
+                {/* Tags like meta, link for CSS, etc. */}
+            </Head>
+            <body>
+            <GoogleTagManager gtmId="GTM-PX3WB949" />
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    );
+}
 
 export default class LexPipeDocument extends Document {
     render() {
