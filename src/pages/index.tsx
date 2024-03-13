85014378ea65f04d6548306368c8b736ef8ca8bd
speedplane <michael.sander@lexpipe.com>
2024-03-12 23:56:53 -0400

    Restore title metadata.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,51 +5,13 @@ import { Header } from '@/components/Header'
 import { HeroAngled } from "@/components/HeroAngled";
 import {FeatureImageManyToMany, FeatureImageHubSpoke} from "@/components/FeatureImageLeft";
 import ValueProposition from "@/components/ValueProposition";
-import Logo1000 from "@/images/LexPipe-White-Sq-630-Pipes.png";
-import type {Metadata} from "next";
-
-// Hard-code the base URL for production environment
-const BASE_URL = 'https://lexpipe.com';
-
-function generateAbsoluteUrl(path: string): string {
-    // Ensure the path does not start with a slash to avoid double slashes in the URL
-    const normalizedPath = path.startsWith('/') ? path.slice(1) : path;
-    return `${BASE_URL}/${normalizedPath}`;
-}
-
-function convertToRelative(url: string): string {
-    // Use a regex to remove the protocol and domain part of the URL
-    // This regex matches any string that starts with a scheme followed by "://",
-    // optionally followed by any characters (non-greedy) until the first slash after the domain
-    const regex = /^[a-zA-Z]+:\/\/[^\/]+/;
-
-    // Replace the matched scheme and domain with an empty string, effectively removing them
-    // Prepend "/" to ensure the result is a relative path, unless the URL is already relative
-    return url.replace(regex, '');
-}
-
-
-export const metadata: Metadata = {
-    title: {
-        template: '%s | LexPipe',
-        default: 'LexPipe: Workflow automation for law firms.',
-    },
-    // Add the logo to the openGraph
-    openGraph : {
-        images: [{
-            url : generateAbsoluteUrl(convertToRelative(Logo1000.src)),
-            width : Logo1000.width,
-            height : Logo1000.height,
-        }],
-    },
-    description:
-        'LexPipe transforms the way law firms and corporates use legal information to develop new business and manage matters.',
-}
-
+import Head from "next/head";
+import {metadata} from "./_app"
 
 export default function Home() {
   return (
     <>
+        <Head><title>{metadata.title.default}</title></Head>
       <Header />
         <main>
             <HeroAngled/>
